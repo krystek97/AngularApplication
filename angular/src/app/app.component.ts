156afbc257677be373198ba5dcb02bba491be8f6
Krystian Kozłowski <34834953+krystek97@users.noreply.github.com>
2018-07-28 22:17:28 +0200

    AngularApplication
    v 1.2

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component
+({
   selector: 'sky-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+
+export class AppComponent 
+{
+  things = [`Hamak` , `Mis` , `Hak` , `Grzebien` , `Sofa`] ; 
+
+  books = new Array<Book>() ; 
+
+  constructor()
+  {
+    this.books.push(new Book(`Alicja w krainie czarow` , 2010) , new Book(`Piotrus Pan` , 2013) , 
+    new Book(`Drzewa w butach` , 2014) )
+  }
 }
+  class Book 
+  {
+    constructor(public name : string , public year : number) 
+    {
+      
+    }
+  }
+
+
+
